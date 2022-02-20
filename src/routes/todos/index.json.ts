551f436e9294ade46a44c99389037bfee31b56a1
Auttakrit Wongsarawit <auttakrit.w@ku.th>
2022-02-20 16:47:24 +0000

    Add GET and POST endpoints

---
+++ src/routes/todos/index.json.ts
@@ -0,0 +1,28 @@
+import type { RequestHandler } from "@sveltejs/kit";
+
+let todos: Todo[] = [];
+
+// get
+export const get: RequestHandler = async ({request}) =>{
+  return {
+    status: 200,
+    body: todos
+  }
+}
+
+// post
+export const post: RequestHandler = async ({request}) =>{
+  const data = await request.formData();
+
+  todos.push({
+    created_at: new Date(),
+    text: data.get('text') as string,
+    done: false
+  })
+  return {
+    status: 303,
+    headers: {
+      location: "/"
+    }
+  }
+}
